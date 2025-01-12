6ccf97b4c752de904de41e9f6d5ecee7d75bc425
Jannis Lübbe <jannis@jl82.de>
2025-01-12 15:42:50 +0100

    added backend for hosting.

---
+++ flood_map/backend.py
@@ -0,0 +1,19 @@
+#!python
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from routers.flood_map import router as flood_map
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(flood_map)
+
+
+@app.get("/", include_in_schema=False)
+async def root(request: Request):
+    return RedirectResponse("/static/index.html")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
